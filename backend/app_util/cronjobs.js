5e5aaf63e9fc77b8e1ffa13323f6e04cbf4fedef
rahulkulmi <rahul@newput.com>
2017-12-07 19:28:42 +0530

    Update cron job url.

--- backend/app_util/cronjobs.js
+++ backend/app_util/cronjobs.js
@@ -6,13 +6,13 @@ var log = require('./logger');
 function startCronJobs() {
 
   var hoursheetJob = crontab.scheduleJob("5 20 * * THU", function() {
-    request('http://localhost:9095/rest/hoursheet/weekly', function (error, response, body) {
+    request('http://34.211.76.6:9095/rest/hoursheet/weekly', function (error, response, body) {
       log.info('Cron hoursheetJob running.');
      });
   });
 
   var timesheetJob = crontab.scheduleJob("0 20 * * THU", function() {
-    request('http://localhost:9095/rest/timesheet/weekly', function (error, response, body) {
+    request('http://34.211.76.6:9095/rest/timesheet/weekly', function (error, response, body) {
       log.info('Cron timesheetJob running.');
      });
   });
